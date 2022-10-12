b2ad4ea32da755cef4f48e8e036edcae955f7400
Mahala Mzati <mahalamkwepu@gmail.com>
2022-10-12 04:05:09 -0700

    chnaged somethings

--- classes/dbh.class.php
+++ classes/dbh.class.php
@@ -1,17 +1,33 @@
 <?php
-class Dbh{
-	private $host = "localhost";
-	private $user = "root";
-	private $pwd = "";
-	private $dbname = "chomora";
+class Dbh
+{
+	private $host;
+	private $user;
+	private $pwd;
+	private $dbname;
 
-	protected function connect(){
-	$dsn = 'mysql:host=' . $this->host . ';dbname='. $this->dbname;
-	$pdo =  new PDO($dsn, $this->user, $this->pwd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	protected function connect()
+	{
+		$this->host = "localhost";
+		$this->user = "u289533873_mikeka";
+		$this->pwd = "8085@Must.Upie";
+		$this->dbname = "u289533873_chomora";
+		$dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+		$pdo =  new PDO($dsn, $this->user, $this->pwd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+		$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-    return $pdo;
+		if (!$pdo) {
+			$this->host = "localhost";
+			$this->user = "root";
+			$this->pwd = "";
+			$this->dbname = "chomora";
+			$dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+			$pdo =  new PDO($dsn, $this->user, $this->pwd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+			$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+			return $pdo;
+		} else {
+			return $pdo;
+		}
 	}
 }
-
-?>
